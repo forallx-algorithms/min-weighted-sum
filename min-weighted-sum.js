d720fcea5ad7d575bba25fd251cb16b94a7655f3
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2015-03-26 18:43:47 +0300

    Ratio greedy score

--- min-weighted-sum.js
+++ min-weighted-sum.js
@@ -5,11 +5,24 @@
   @date 26.03.2015
 */
 
-// Calculate greedy score of an data instance using difference
+// TODO: ties, ration, calc completion time, refactor, fread
+
+// Calculate greedy score of a data instance using difference
 // @param {Array.<Integer, Integer>} d
-// @return {Float}
+// @return {Integer}
 function calcScoreDiff(d) {
   return d[0] - d[1];
+}
+
+// Calculate greedy score of a data instance using ratio
+// @param {Array.<Integer, Integer>} d
+// @return {Float}
+function calcScoreRation(d) {
+  if(d[1] == 0) {
+    return 0;
+  } else {
+    return d[0]/d[1];
+  }
 }
 
 // Calculate min weighted sum order
@@ -27,9 +40,8 @@ function calcMinWeightedOrder(d, diffFn) {
 // section: Tests
 
 var dataSample = [
-  [74, 59],
-  [31, 73],
-  [45, 79]
+  [3,5],
+  [1,2]
 ];
 
 console.log("Case 1:", calcScoreDiff(dataSample[0])==(dataSample[0][0]-dataSample[0][1]), calcScoreDiff(dataSample[0]));
@@ -37,8 +49,17 @@ console.log("Case 1:", calcScoreDiff(dataSample[0])==(dataSample[0][0]-dataSampl
 function test2(){
   var r = calcMinWeightedOrder(dataSample);
 
-  return r[0][0] == 74 && r[1][0] == 45 && r[2][0] == 31;
+  return r[0][0] == 1 && r[1][0] == 3;
 }
 console.log("Case 2:", test2());
+
+console.log("Case 3:", calcScoreRation(dataSample[0])==dataSample[0][0]/dataSample[0][1]);
+
+function test4(){
+  var r = calcMinWeightedOrder(dataSample, calcScoreRation);
+
+  return r[0][0] == 3 && r[1][0] == 1;
+}
+console.log("Case 4:", test4());
 
 
